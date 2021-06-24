6424b3382e6db1e1400a445de6ba51303d8df83b
Allan Cantillo <allan.cantillo@mobilize.net>
2021-06-24 13:54:19 -0600

    Unifying scripts to handle mload/bteq to extract and restore in a single script for each action, avoiding duplicated logic.

--- extract_snippets.py
+++ extract_snippets.py
@@ -1,59 +1,124 @@
-import os
-import sys
-import re
-
 import argparse
+import os
+import re
+from shutil import copyfile
 
-
-arguments_parser = argparse.ArgumentParser(description="BTEQ embeded shell script extractor for SnowConvert")
+arguments_parser = argparse.ArgumentParser(description="MLOAD/BTEQ embedded shell script extractor for SnowConvert")
 arguments_parser.add_argument('--inputdir',required=True, help='This is the directory where your *.sh or *.ksh files are')
 arguments_parser.add_argument('--outdir', required=True, help='This is the directory where the splitted files will be put')
 arguments = arguments_parser.parse_args()
 
+supported_extensions = { "bteq":"_BTEQ.py", "mload": "_MultiLoad.py" }
+pattern_extensions = "|".join(supported_extensions.keys())
+pattern_extensions_upper = pattern_extensions.upper()
 input_directory = arguments.inputdir
 output_directory = arguments.outdir
+snippetbyext = {}
+unmodifiedfiles = 0
+totalfiles = 0
+PRE_SH_TYPE = "pre.sh"
+supported_extension_keys = supported_extensions.keys()
+supported_extension_tags = []
 
-def findnext(lines,pos,terminator,block):
+for ext in supported_extension_tags:
+    supported_extension_tags.append("$" + ext.upper() + "_COMMAND")
+
+def find_extensions_in_text(line, extensions, extension_tags):
+    for ext in extensions:
+        if ext in line:
+            return True
+
+    for tag in extension_tags:
+        if tag in line:
+            return True
+
+    return False
+
+def findnext(lines, pos, terminator, block):
     pos = pos + 1
     while (pos < len(lines)):
         line = lines[pos]
-        if re.search(f".*{terminator}.*",line):
+        if re.search(f".*{terminator}.*", line):
             return pos + 1
-        block.append(line + "\n")
+        replaced_text = re.sub(r'^([ \t]*)(\$[A-Za-z][A-Za-z0-9]*;[ \t]*)$', r'\1;/*Not supported command from variable \2*/', line)
+        block.append(replaced_text + "\n")
         pos = pos + 1
     return pos
 
+if (len(input_directory) > 0 and input_directory[-1] != os.path.sep):
+    input_directory = input_directory + os.path.sep
 
+rootlen = len(input_directory)
 for dirpath, dirnames, files in os.walk(input_directory):
     for file_name in files:
-        print(file_name)
-        all_text = open(os.path.join(dirpath, file_name), encoding="ISO-8859-1").read()
-        if not "bteq" in all_text and not "$BTEQ_COMMAND" in all_text:
-            continue
-        lines = all_text.splitlines()
+        inputfile = os.path.join(dirpath, file_name)
+        inputsubdir = os.path.dirname(inputfile)
+        subdir = inputsubdir[rootlen:]
+        all_text = open(inputfile, encoding="ISO-8859-1").read()
+
         snippets = []
-        file_without_snippets = []
-        pos = 0
-        while pos < len(lines):
-            current_line = lines[pos]
-            matches = re.match(".*bteq\s*<<[-]*\s*(.+?)\s.*",current_line) or re.match(".*bteq\s*<<[-]*\s*(.*)$",current_line) or re.match(".*\$BTEQ_\w+\s*<<[-]*\s*(.*)$",current_line)
-            if matches:
-                terminator = matches.group(1)
-                newblock = []
-                pos = findnext(lines,pos,terminator,newblock)
-                snippets.append(newblock)
-                file_without_snippets.append(f"@@SNIPPET{len(snippets)}\n")
-                continue
-            file_without_snippets.append(current_line + "\n")
-            pos = pos + 1
 
-        snippetName = os.path.join(output_directory, file_name)
-        with open(snippetName + ".pre.sh","w", encoding="ISO-8859-1") as newscript:
+        if find_extensions_in_text(all_text, supported_extension_keys, supported_extension_tags):
+            lines = all_text.splitlines()
+            file_without_snippets = []
+
+            pos = 0
+            lenLines = len(lines)
+            while pos < lenLines:
+                current_line = lines[pos]
+                matches = re.match(f".*({pattern_extensions})\s*<<[-]*\s*(.+?)\s.*", current_line) or re.match(f".*({pattern_extensions})\s*<<[-]*\s*(.*)$", current_line) or re.match(f".*\$({pattern_extensions_upper})_\w+\s*<<[-]*\s*(.*)$", current_line)
+                if matches:
+                    terminator = matches.group(2)
+                    filetype = matches.group(1).lower()
+                    newblock = []
+                    pos = findnext(lines, pos, terminator, newblock)
+                    snippets.append(newblock)
+                    file_without_snippets.append(f"@@SNIPPET{len(snippets)}{supported_extensions[filetype]}\n")
+                    continue
+                file_without_snippets.append(current_line + "\n")
+                pos = pos + 1
+
+        outputfile = os.path.join(output_directory, subdir, file_name)
+        outputsubdir = os.path.dirname(outputfile)
+        if not os.path.exists(outputsubdir):
+            os.makedirs(outputsubdir)
+
+        if len(snippets) == 0:
+            unmodifiedfiles = unmodifiedfiles + 1
+            totalfiles = totalfiles + 1
+            try:
+                copyfile(inputfile, outputfile)
+            except IOError as e:
+                print(f"Error: Unable to copy file. {e}")
+            continue
+
+        with open(outputfile + ".pre.sh", "w", encoding="ISO-8859-1") as newscript:
             newscript.writelines(file_without_snippets)
+        print(f"Wrote to file {subdir}{os.path.sep}{file_name}.pre.sh without snippets")
 
-        index = 1
+        currentpresh = 0
+        currentsumsnippets = 0
+
+        if filetype in snippetbyext:
+            currentsumsnippets = snippetbyext[filetype]
+        snippetbyext[filetype] = currentsumsnippets + len(snippets)
+        totalfiles = totalfiles + len(snippets)
+
+        if PRE_SH_TYPE in snippetbyext:
+            currentpresh = snippetbyext[PRE_SH_TYPE]
+        snippetbyext[PRE_SH_TYPE] = currentpresh + 1
+        totalfiles = totalfiles + 1
+        
+        pos = 1
         for s in snippets:
-            with open(f"{snippetName}.snippet.{index}.bteq","w",  encoding="ISO-8859-1") as newsnippet:
+            outputsuffix = f".snippet.{pos}.{filetype}"
+            with open(f"{outputfile}{outputsuffix}", "w", encoding="ISO-8859-1") as newsnippet:
                 newsnippet.writelines(s)
-            index = index + 1
-        print("done")
+            pos = pos + 1
+            print(f"Wrote to file {subdir}{os.path.sep}{file_name}{outputsuffix}")
+
+print()
+print("The total of created files")
+print(snippetbyext)
+print(f"The total of copied unmodified files {unmodifiedfiles}")
+print(f"Total output files {totalfiles}")
--- restore_snippets.py
+++ restore_snippets.py
@@ -1,37 +1,41 @@
-import os
-import sys
-import re
-
-
 import argparse
+import os
+import re
+from os.path import exists
 
-
-arguments_parser = argparse.ArgumentParser(description="BTEQ embedded shell script restorer for SnowConvert. This script will take the migrated snippets and recreated shell scripts using those files")
+arguments_parser = argparse.ArgumentParser(description="MLOAD/BTEQ embedded shell script restorer for SnowConvert. This script will take the migrated snippets and recreated shell scripts using those files")
 arguments_parser.add_argument('--inputdir',required=True, help='This is the directory where your *.sh or *.ksh files are')
 arguments = arguments_parser.parse_args()
 
 input_directory = arguments.inputdir
-
+totalfiles = 0
 
 for dirpath, dirnames, files in os.walk(input_directory):
     for file_name in files:
         if "pre.sh" in file_name:
-            file_path = os.path.join(dirpath,file_name)
+            totalfiles = totalfiles + 1
+            file_path = os.path.join(dirpath, file_name)
             if "pre.sh" in file_name:
-                original_file = re.sub("(.*).pre.sh",r"\1",file_path)
+                original_file = re.sub("(.*).pre.sh", r"\1", file_path)
                 target_script = original_file + ".sh"
-            if "pre.ksh" in file_name:
-                original_file = re.sub("(.*).pre.sh",r"\1",file_path)
-                target_script = original_file + ".ksh"
-            lines = open(file_path).readlines()
-            print(target_script)
-            with open(target_script,"w") as f:
-                for l in lines:
-                    matches = re.search("@@SNIPPET(\d+)",l)
-                    if not matches is None:
-                        snippet_number = matches.groups()[0]
-                        with open(original_file + ".snippet." + snippet_number + "_BTEQ.py") as snippet_file:
-                            contents = snippet_file.read()
-                            f.write(f'result=$(python <<END_SNOWSCRIPT\n{contents}\nEND_SNOWSCRIPT\n)\n')
+            lines = open(file_path, encoding="ISO-8859-1").readlines()
+            print(f"Restoring {target_script}")
+            with open(target_script, "w", encoding="ISO-8859-1") as file:
+                for current_line in lines:
+                    matches = re.search("@@SNIPPET(\d+)(.*)$", current_line)
+                    if matches is not None:
+                        snippet_number = matches.group(1)
+                        suffix_file = matches.group(2)
+                        snippet_filepath = original_file + ".snippet." + snippet_number + suffix_file
+                        if exists(snippet_filepath):
+                            with open(snippet_filepath, encoding="ISO-8859-1") as snippet_file:
+                                contents = snippet_file.read()
+                                file.write(f'result=$(python <<END_SNOWSCRIPT\n{contents}\nEND_SNOWSCRIPT\n)\n')
+                        else:
+                            print(f"The expected snippet file {snippet_filepath} does not exist")
                     else:
-                        f.write(l)
+                        file.write(current_line)
+
+print()
+print(f"The total of sh restored files are {totalfiles}")
+
